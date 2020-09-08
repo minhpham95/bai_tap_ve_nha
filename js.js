e368b3502cc8d98e72f1b49f041bd5b0dd49fadd
Damon PHAM <da.pham@linkbynet.com>
2020-09-08 18:04:43 +0700

    init

---
+++ js.js
@@ -0,0 +1,20 @@
+﻿$(function(){
+    $('.noidung').slideUp();
+	$('.motkhoi .title').click(function(event) {
+		
+		$(this).next().slideToggle();
+		$(this).toggleClass('xanh')
+    });
+    
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 1) {
+          $('#top-header').addClass('fixed');
+         }
+         else {
+          $('#top-header').removeClass('fixed');
+         }
+      });
+})  
+
+
+ 
